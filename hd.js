00e5adbb9308114239a1e445f656b43a1a41d61f
i5ting <i5ting@126.com>
2016-07-20 22:24:39 +0800

    lazycommmit, please ignore

--- hd.js
+++ hd.js
@@ -1,5 +1,9 @@
 var c = require('.')
 
-c(0, 330, function (current, count) {
-  console.log(current + '/' + count)
-})
+// c(0, 330, function (current, count) {
+//   console.log(current + '/' + count)
+// }, 'dist2')
+  
+  c(0, 330, function (current, count) {
+    console.log(current + '/' + count)
+  })
